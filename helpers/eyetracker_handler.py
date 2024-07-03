6fc062a71b6e9ebe5353fe20647712e6b446ed94
bitesizing <r.raimundo2001@gmail.com>
2024-07-03 10:04:01 +0100

    . Added docstrings to eyetracker class

--- helpers/eyetracker_handler.py
+++ helpers/eyetracker_handler.py
@@ -5,7 +5,16 @@ from tobiiresearch.implementation.EyeTracker import EyeTracker, find_all_eyetrac
 from . import write_file
 
 class EyetrackerHandler():
+    """
+    Handler class for the eyetracker.
+
+    Args:
+        subscriptions_list (list): List of subscriptions to record.
+        parent_folder (str, optional): Parent folder to save data in. Defaults to 'datafiles'.
+        individual_folder (str, optional): Individual folder to save data in. Defaults to None.
+    """
     def __init__(self, subscriptions_list: list, parent_folder: str = 'datafiles', individual_folder: str = None):
+        # Default to current timestamp in seconds
         if individual_folder is None: individual_folder = str(int(time.time()))
 
         self.parent_folder, self.individual_folder = parent_folder, individual_folder
@@ -14,18 +23,26 @@ class EyetrackerHandler():
         self.data_dict = {subscription_name: [] for subscription_name in subscriptions_list}
 
     def setup_eyetracker(self) -> EyeTracker | None:
-        """ Setup function that will 'find' the eyetracker if it exists (is plugged into the PC)"""
+        """
+        Setup function that will 'find' the eyetracker if it exists (is plugged into the PC).
+
+        Returns:
+            EyeTracker: The found eyetracker.
+        """
         all_trackers = find_all_eyetrackers()
         return(all_trackers[0])
 
-    def subscribe(self):
+    def subscribe(self) -> None:
+        """ Subscribe to the given subscriptions. """
         for subscription_name in self.subscriptions_list:
             self.eyetracker.subscribe_to(subscription_name, self.data_dict[subscription_name].append, as_dictionary=True)
 
     def unsubscribe(self):
+        """ Unsubscribe from the given subscriptions. """
         for subscription_name in self.subscriptions_list:
             self.eyetracker.unsubscribe_from(subscription_name)
 
     def write_files(self):
+        """ Write the recorded data as pickle files in the selected folder. """
         for subscription_name, data in self.data_dict.items():
             write_file(f'{self.parent_folder}/{self.individual_folder}/{subscription_name}.pkl', data=data)
