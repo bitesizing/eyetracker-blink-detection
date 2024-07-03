df4032af8d4055479e63b3ff99dabbce98b6a906
bitesizing <r.raimundo2001@gmail.com>
2024-07-03 10:00:28 +0100

    . Added comments to record_eyetracker

--- record_eyetracker.py
+++ record_eyetracker.py
@@ -1,18 +1,18 @@
 # %%
 """ File to record from the eyetracker for a given period of time, and save the data. """
-
-# 'Import' code (pull in code that tobii etc. have written so we can use it)
 import time
 from helpers import EyetrackerHandler
 
-# Fill out manually - all subscriptions listed in subscriptions_list.txt
-subscriptions_list = ['eyetracker_gaze_data']
+# Manually list all the data sources to record - options are listed in `helper-files/subscriptions_list.txt`
+subscriptions_list = [
+    'eyetracker_gaze_data',
+    ]
 
-# Setup eyetracker, record for 2s, then sleep
-eyetracker = EyetrackerHandler(subscriptions_list)
-eyetracker.subscribe()
-time.sleep(10)
-eyetracker.unsubscribe()
+# Toy code to set up the eyetracker, record for 2s, then sleep
+eyetracker = EyetrackerHandler(subscriptions_list)  # set up the eytracker as an 'object' with the given subscriptions
+eyetracker.subscribe()  # start recording
+time.sleep(10)  # wait 10s
+eyetracker.unsubscribe()  # stop recording
 
 """ Save eye images and gaze data to json files. """
 eyetracker.write_files()
